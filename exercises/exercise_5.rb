3ab26911a59727ac1dc0e782d07f06164c71def4
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-06-28 12:34:42 -0700

    implement e3, e4, e5, e6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum = Store.sum(:annual_revenue)
+@avg = Store.average(:annual_revenue)
+@overMCount = Store.where("annual_revenue > ?", 1000000).count
+
+puts "Sum of Annual Revenues: #{@sum}"
+
+puts "Average of Annual Revenues: #{@avg}"
+
+puts "Number of Annual Revenues more than 1M: #{@overMCount}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# Store[:has_many] = employees
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alex", last_name: "Lu", hourly_rate: 40)
+@store2.employees.create(first_name: "Sarah", last_name: "Free", hourly_rate: 15)
+@store2.employees.create(first_name: "Mina", last_name: "One", hourly_rate: 50)
+
+p Employee.count
